7a419b201fdfaf5acfca8185a2833df31d3d9e7d
Nikolay Dozmorov <ndozmorov@gmail.com>
2019-10-15 00:00:05 +0700

    Add message about empty abstract

--- src/templates/publications.js
+++ src/templates/publications.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { FormattedMessage } from "react-intl"
 
 import { Layout } from "../components/layout"
 import { getLocalizedNodes } from "../intl/utils"
@@ -31,7 +32,18 @@ export default ({ data, pageContext: { locale } }) => {
       <article className="publication">
         <h1 dangerouslySetInnerHTML={{ __html: title }} />
         <div className="publication__card card card--left">
-          <div className="publication__abstract" dangerouslySetInnerHTML={{ __html: html }} />
+          <div className="publication__abstract">
+            {
+              html.length > 0
+              &&
+              <div dangerouslySetInnerHTML={{ __html: html }} />
+            }
+            {
+              html.length === 0
+              &&
+              <p><FormattedMessage id="publication-page.no-abstract" /></p>
+            }
+          </div>
           <h4 className="publication__authors">{author}</h4>
           <div className="publication__journal">{journal} {year}, {volume}{issue && (", " + issue)} </div>
           <a href={"https://doi.org/" + doi} target="_blank" rel="noopener noreferrer">{doi}</a>
